50170d8807f4170cf97cec2478e9b9732e730a66
sepehr ariamehr <sepehrariamehr@hotmail.com>
2020-08-19 03:57:09 +0430

    rahat shodam

--- Sandogh.App/MainWindow.xaml.cs
+++ Sandogh.App/MainWindow.xaml.cs
@@ -1,25 +1,10 @@
 ﻿
-using MaterialDesignThemes.Wpf;
-
 using Sandogh.DataLayer.Context;
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using System.Globalization;
-using PersianCalendarWPF;
 using System.Threading;
-using System.Windows.Markup;
+using System.Windows;
+using System.Windows.Input;
 
 namespace Sandogh.App
 {
@@ -31,34 +16,37 @@ namespace Sandogh.App
         public MainWindow()
         {
             InitializeComponent();
+            this.GetUsers();
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            GetUsers();
+            Thread thread = new Thread(this.GetUsers);
         }
         private void GetUsers()
-        {
+        {            
             using (UnitOfWork a = new UnitOfWork())
                 DgvUsers.ItemsSource = a.UserRepository.GetAllUser();
         }
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            GetUsers();
+           // Thread thread = new Thread(this.GetUsers);
 
         }
 
         private void MainWindow_Initialized(object sender, EventArgs e)
         {
             this.Dispatcher.InvokeAsync(() =>
-{
-    LoginWindow L = new LoginWindow();
-    {
+            {
 
-        L.ShowDialog();
-        Visibility = Visibility.Visible;
-    }
-});
+
+                LoginWindow L = new LoginWindow();
+                {
+                    L.ShowDialog();
+                    Visibility = Visibility.Visible;
+
+                }
+            });
         }
 
 
